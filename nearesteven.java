809201765c78f52390148b65f873d256e489f979
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-22 13:13:07 +0530

    Create nearesteven.java

---
+++ nearesteven.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int ans=0;
+		if(n%2==0)
+		{
+			ans=n-2;
+			System.out.println(ans);
+		}
+		else
+		{
+			ans=n-1;
+			System.out.println(ans);
+		}
+	}
+}
