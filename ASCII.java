8a1022332b0806dfeb685e0e34b07909486cb6db
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-23 12:22:33 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		char c=s.next().charAt(0);
+	    int ch=c;
+	    System.out.println(ch);
+	}
+}
