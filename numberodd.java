c7e09f2ac251d2ed0242f3010308a1597db24af7
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-27 11:02:57 +0530

    Create numberodd.java

---
+++ numberodd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+                 Scanner s=new Scanner(System.in);
+                 String str=s.nextLine();
+                 int l=0;
+                 for(int i=0;i<str.length();i++)
+                 {
+                 	if(str.charAt(i)%2!=0)
+                 	{
+                 		System.out.print(str.charAt(i)+" ");
+                         }               	
+                 }     
+                 	
+                 		
+                 
+	}
+}
