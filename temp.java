69bff969bca720007185f1c15517a167999c42ae
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-29 10:38:24 +0530

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int c=s.nextInt();
+		double n=0;
+		n=c+273.15;
+		System.out.println((int)n);
+	}
+}
