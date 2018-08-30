4578ba58a82ccfbb2d2f11164c16b930c2de6a07
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-30 10:23:31 +0530

    Update abcnum.java

--- abcnum.java
+++ abcnum.java
@@ -8,9 +8,9 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
-		int a=s.nextInt();
-		int b=s.nextInt();
-		int c=s.nextInt();
-		System.out.println((a*b)/c);
+		int A=s.nextInt();
+		int B=s.nextInt();
+		int C=s.nextInt();
+		System.out.println((A*B)/C);
 	}
 }
