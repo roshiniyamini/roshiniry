b2b98c10c5169e8d2fa72e0844691c5a4042c70d
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-30 10:21:57 +0530

    Rename abc.java to abcnum.java

---
+++ abcnum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		System.out.println((a*b)/c);
+	}
+}
